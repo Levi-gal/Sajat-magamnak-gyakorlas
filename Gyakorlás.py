dd733cf70a80a18d6bf0201345d6b008db275ad3
Levi-gal <90601416+Levi-gal@users.noreply.github.com>
2021-10-23 12:56:42 +0200

    Update Gyakorlás.py

--- Gyakorlás.py
+++ Gyakorlás.py
@@ -1,10 +1,25 @@
 import random
-while True:
-   input("Nyomj egy ENTERT a dobáshoz")
+szavak = ["Arany tömb" , "Gyémánt" , "Cseresznye" , "Pénzes zsák" , "Kard"]
+int(input("Üss be egy 1-est a kezdéshez "))
 
-   num = random.randint(1,11)
-   print("A szám amit kaptál:",num)
-   option = input("Újra dobsz?(Igen/Nem) ")
+num1 = random.choice(szavak)
+num2 = random.choice(szavak)
+num3 = random.choice(szavak)
+print("Az tárgy amit kaptál:",num1)
+print("Az tárgy amit kaptál:",num2)
+print("Az tárgy amit kaptál:",num3)
 
-   if option == 'Nem':
-       break
+if(num1 == num2 == num3):
+    print("Max nyeremény: 5000Ft")
+
+elif(num1 == num2):
+    print("Közepes nyeremény: 2500Ft")
+
+elif(num1 == num3):
+    print("Közepes nyeremény: 2500Ft")
+
+elif(num2 == num3):
+    print("Közepes nyeremény: 2500Ft")
+
+else:
+    print("Nem nyert")
