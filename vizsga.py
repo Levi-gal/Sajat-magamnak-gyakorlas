3fbd117e4dbc23ba02b727b9fcdeb59a2b1cfc58
Levi-gal <90601416+Levi-gal@users.noreply.github.com>
2022-05-11 13:02:13 +0200

    Add files via upload

---
+++ vizsga.py
@@ -0,0 +1,16 @@
+from cgi import print_directory
+from re import I, T
+
+
+print('Elektromos teljesítmény és ellenállás számolás, bekért feszültség és áramerősség értékét egy tetszőleges változóba!')
+
+while True:
+
+ feszültség = int(input('Add meg a feszültség változóját! '))
+ áramerősség = int(input('Add meg a áramerősség változóját! '))
+ 
+ P=u*i
+ R=u/i
+
+
+ 
